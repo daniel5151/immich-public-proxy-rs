eb9179cfdfdd9ef407aafc2e26f36bac7510398b
Daniel Prilik <danielprilik@gmail.com>
2026-06-13 10:44:05 -0400

    fix: prevent browser intervention warning on non-cancelable touchmove

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Prevent Chrome intervention warnings when preventDefault() is called on non-cancelable events (like touchmove during scroll)
+const originalPreventDefault = Event.prototype.preventDefault;
+Event.prototype.preventDefault = function (this: Event) {
+  if (this.cancelable !== false) {
+    originalPreventDefault.call(this);
+  }
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
